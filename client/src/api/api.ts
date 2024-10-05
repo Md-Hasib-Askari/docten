0d577951e8e869dfe1c3569e2753e92910883ebe
asif1416 <ashrafulasif260@gmail.com>
2024-10-05 16:36:54 +0600

    axios added

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:5000/api/v1';
 
-// Function to register a new user
+// Register a new user
 export const registerUser = async (email: string, password: string, confirmPassword: string, role: string) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/register`, {
@@ -13,11 +13,11 @@ export const registerUser = async (email: string, password: string, confirmPassw
     });
     return response.data;
   } catch (error: any) {
-    throw new Error(error.response?.data?.data || 'Registration failed. Please try again.');
+    throw new Error(error.response?.data?.data );
   }
 };
 
-// Function to login a user
+// Login a user
 export const loginUser = async (email: string, password: string) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/login`, {
@@ -26,7 +26,41 @@ export const loginUser = async (email: string, password: string) => {
     });
     return response.data;
   } catch (error: any) {
-    throw new Error(error.response?.data?.data || 'Login failed. Please try again.');
+    throw new Error(error.response?.data?.data);
+  }
+};
+
+
+//Send OTP to the given email
+export const sendOtp = async (email: string) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/forgot-password/send-otp`, { email });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.data );
+  }
+};
+
+// Verify OTP for the given email
+export const verifyOtp = async (email: string, otp: string) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/forgot-password/verify-otp`, { email, otp });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.data);
+  }
+};
+
+//Reset password
+export const resetPassword = async (email: string, newPassword: string) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/forgot-password/reset-password`, {
+      email,
+      newPassword,
+    });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.data);
   }
 };
 
--- server/src/Routes/authRoutes.ts
+++ server/src/Routes/authRoutes.ts
@@ -12,7 +12,8 @@ router.post('/logout', authController.logoutUser);
 
 // Password Reset Routes
 router.post('/forgot-password/send-otp', forgetPassController.sendOtp); // Send OTP
-router.post('/forgot-password/verify-otp', forgetPassController.verifyOtpHandler); // Verify OTP
+router.post('/forgot-password/verify-otp', forgetPassController.verifyOtp); // Verify OTP
 router.post('/forgot-password/reset-password', forgetPassController.resetPassword); // Reset Password
 
 module.exports = router;
+     
