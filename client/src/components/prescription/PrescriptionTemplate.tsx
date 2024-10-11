edd7ebdcfda4ce19d5a61e67b562c79afdbf4481
hasib <mdhsbaskari@gmail.com>
2024-10-12 00:31:06 +0600

    Added editable prescription doc, added temporary data prescription.json

--- client/src/components/prescription/PrescriptionTemplate.tsx
+++ client/src/components/prescription/PrescriptionTemplate.tsx
@@ -1,54 +1,92 @@
-import Image from "next/image";
-import { ClipboardList, FileText, Stethoscope, Activity } from "lucide-react";
-import { forwardRef, ReactNode } from "react";
+import {
+  ClipboardList,
+  FileText,
+  Stethoscope,
+  Activity,
+  EditIcon,
+  PlusIcon,
+} from "lucide-react";
+import { forwardRef, ReactNode, useEffect, useState } from "react";
+import PrescriptionHeader from "@/components/prescription/PrescriptionHeader";
+import axios from "axios";
+import { Button } from "@nextui-org/react";
+import { CloseIcon } from "@nextui-org/shared-icons";
+import { useDashboardStore } from "@/store/dashboardStore";
 
 const PrescriptionTemplate = forwardRef<HTMLDivElement, { isPrint?: boolean }>(
   ({ isPrint = false }: { isPrint?: boolean }, ref) => {
+    const [complaints, setComplaints] = useState<
+      {
+        complaint: string;
+        duration: string;
+        severity: string;
+        description: string;
+      }[]
+    >([]);
+    const [history, setHistory] = useState<
+      {
+        duration: string;
+        description: string;
+        diagnosis: string;
+      }[]
+    >([]);
+    const [diagnosis, setDiagnosis] = useState<
+      {
+        name: string;
+        description: string;
+        date: string;
+      }[]
+    >([]);
+    const [investigation, setInvestigation] = useState<
+      {
+        name: string;
+        description: string;
+        date: string;
+      }[]
+    >([]);
+    const [medicines, setMedicines] = useState<
+      {
+        type: string;
+        name: string;
+        dosage: string;
+        duration: string;
+        frequency: string;
+      }[]
+    >([]);
+    const [instructions, setInstructions] = useState<
+      {
+        id: number;
+        instruction: string;
+      }[]
+    >([]);
+    const [followUp, setFollowUp] = useState<{
+      date: string;
+      time: string;
+    } | null>(null);
+
+    const isEditable = useDashboardStore((state) => state.isEditable);
+
+    useEffect(() => {
+      (async () => {
+        const res = await axios.get("/prescription.json");
+        if (res.data) {
+          setComplaints(res.data.complaints);
+          setHistory(res.data.history);
+          setDiagnosis(res.data.diagnosis);
+          setInvestigation(res.data.investigations);
+          setMedicines(res.data.medicines);
+          setInstructions(res.data.instructions);
+          setFollowUp(res.data.followUp);
+        }
+      })();
+    }, []);
     return (
       <div
         ref={ref}
         id="print-precription"
         className={`${isPrint ? "w-[210mm] h-[297mm]" : "w-full h-full"} mx-auto p-8 rounded-lg bg-white shadow-lg print:shadow-none`}
       >
-        <div className="flex justify-between items-start mb-6">
-          <div>
-            <h1 className="text-2xl font-bold text-green-600">
-              Dr Rashid Khan
-            </h1>
-            <p className="text-sm text-gray-600">MBBS, FCPS, PGT (Diploma)</p>
-            <p className="text-sm font-semibold text-green-600">
-              Assistant professor, Medicine
-            </p>
-            <p className="text-sm text-gray-600">
-              Popular diagnostic center and hospital, Mirpur 10
-            </p>
-            <p className="text-sm text-gray-600">Email: doctor@example.com</p>
-            <p className="text-sm text-gray-600">
-              Cell: +01293347324, +01293347324,
-            </p>
-            <p className="text-sm text-gray-600">BMDC: D-4874</p>
-          </div>
-          <Image
-            src="/placeholder.svg"
-            alt="Hospital Logo"
-            width={80}
-            height={80}
-            className="bg-green-600"
-          />
-        </div>
-
-        <div className="border-t border-b border-gray-300 py-2 mb-4 flex flex-wrap justify-between text-sm">
-          <div>
-            <span className="font-semibold">Patient:</span> Abdus Sattar Rahim
-            <span className="ml-4 font-semibold">Age:</span> 28 years
-            <span className="ml-4 font-semibold">Weight:</span> 53kg
-          </div>
-          <div>
-            <span className="ml-4 font-semibold">Date:</span> 13 September, 2022
-            <span className="ml-4 font-semibold">Time:</span> 10:20 am
-          </div>
-        </div>
-
+        <PrescriptionHeader />
         <div className="flex">
           <div className="w-1/3 pr-4">
             <Section
@@ -56,19 +94,22 @@ const PrescriptionTemplate = forwardRef<HTMLDivElement, { isPrint?: boolean }>(
               icon={<ClipboardList className="w-4 h-4" />}
             >
               <ul className="list-disc pl-5 text-sm">
-                <ListItem description="some description">
-                  Chest pain & excretion (for 5 days)
-                </ListItem>
-                <ListItem>No angina (for 10 days)</ListItem>
-                <ListItem>Tiredness (for 1 month)</ListItem>
+                {complaints.map((complaint, index) => (
+                  <ListItem description={complaint.description} key={index}>
+                    {complaint.complaint} ({complaint.duration}) -{" "}
+                    {complaint.severity}
+                  </ListItem>
+                ))}
               </ul>
             </Section>
 
             <Section title="History" icon={<FileText className="w-4 h-4" />}>
               <ul className="list-disc pl-5 text-sm">
-                <ListItem>CUD (for 4 days)</ListItem>
-                <ListItem>HTN/CAD (for 10 days)</ListItem>
-                <ListItem>Cholesterol (for 1 month)</ListItem>
+                {history.map((item, index) => (
+                  <ListItem description={item.description} key={index}>
+                    {item.description} (for {item.duration})
+                  </ListItem>
+                ))}
               </ul>
             </Section>
 
@@ -77,8 +118,14 @@ const PrescriptionTemplate = forwardRef<HTMLDivElement, { isPrint?: boolean }>(
               icon={<Stethoscope className="w-4 h-4" />}
             >
               <ul className="list-disc pl-5 text-sm">
-                <ListItem>Post anginal</ListItem>
-                <ListItem>Lumbar puncture</ListItem>
+                {diagnosis.map((item, index) => (
+                  <ListItem key={index}>
+                    {item.name} -{" "}
+                    <span className="text-[11px] text-gray-600">
+                      ({item.date})
+                    </span>
+                  </ListItem>
+                ))}
               </ul>
             </Section>
 
@@ -87,50 +134,61 @@ const PrescriptionTemplate = forwardRef<HTMLDivElement, { isPrint?: boolean }>(
               icon={<Activity className="w-4 h-4" />}
             >
               <ul className="list-disc pl-5 text-sm">
-                <ListItem>CBC</ListItem>
-                <ListItem>Blood grouping</ListItem>
-                <ListItem>RBC Count</ListItem>
+                {investigation.map((item, index) => (
+                  <ListItem description={item.description} key={index}>
+                    {item.name} -{" "}
+                    <span className="text-[11px] text-gray-600">
+                      ({item.date})
+                    </span>
+                  </ListItem>
+                ))}
               </ul>
             </Section>
           </div>
 
+          {/* Medication Corner */}
           <div className="w-2/3 pl-4 border-l border-gray-300">
-            <h2 className="text-lg font-semibold mb-2 flex items-center">
-              <FileText className="w-5 h-5 mr-2" />
-              Medicine (Rx)
-            </h2>
+            <div className="flex flex-row gap-3">
+              <h2 className="text-lg font-semibold mb-2 flex items-center">
+                <FileText className="w-5 h-5 mr-2" />
+                Medicine (R<sub>x</sub>)
+              </h2>
+              {isEditable && (
+                <Button
+                  className="min-w-6 h-6 p-0 text-white bg-success"
+                  startContent={<PlusIcon size="20" className="font-black" />}
+                />
+              )}
+            </div>
             <div className="space-y-4">
-              <Medication
-                type="Tab."
-                name="Pantonix 40 mg"
-                dosage="1+0+1"
-                duration="14 দিন"
-                quantity="30 Pcs"
-              />
-              <Medication
-                type="Inj."
-                name="Oseotril 100 mg (Capsule)"
-                dosage="1+0+1"
-                duration="সপ্তাহে ৩ বার"
-                quantity="14 Pcs"
-              />
-              <Medication
-                type="Tab."
-                name="Fenofex 40 mg"
-                dosage="1+0+1 খাবার পর"
-                duration="14 দিন"
-                quantity="30 Pcs"
-              />
+              {medicines.map((medicine, index) => (
+                <Medication
+                  key={index}
+                  type={medicine.type}
+                  name={medicine.name}
+                  dosage={medicine.dosage}
+                  duration={medicine.duration}
+                  frequency={medicine.frequency}
+                />
+              ))}
             </div>
 
             <div className="mt-6">
-              <h3 className="font-semibold mb-1">Advice</h3>
-              <p className="text-sm">Follow the prescription carefully.</p>
+              <Header title="Instructions" />
+              {
+                <ol className="pl-5 list-decimal text-sm">
+                  {instructions.map((item, index) => (
+                    <ListItem key={index}>{item.instruction}</ListItem>
+                  ))}
+                </ol>
+              }
             </div>
 
             <div className="mt-4">
-              <h3 className="font-semibold mb-1">Follow up</h3>
-              <p className="text-sm">After 2 weeks</p>
+              <Header title="Follow Up" />
+              <p className="text-sm">
+                <span className="underline">Next Visit:</span> {followUp?.date}
+              </p>
             </div>
           </div>
         </div>
@@ -148,13 +206,39 @@ export function Section({
   children: ReactNode;
   icon?: ReactNode;
 }) {
+  const isEditable = useDashboardStore((state) => state.isEditable);
+
   return (
     <div className="mb-4">
       <h2 className="text-md font-semibold mb-2 flex items-center">
         {icon}
-        <span className="ml-2">{title}</span>
+        <div className="flex flex-row gap-3 items-center">
+          <span className="ml-2">{title}</span>
+          {isEditable && (
+            <Button
+              className="min-w-6 h-6 p-0 text-white bg-success"
+              startContent={<PlusIcon size="20" className="font-black" />}
+            />
+          )}
+        </div>
       </h2>
       {children}
+    </div>
+  );
+}
+
+function Header({ title }: { title: string }) {
+  const isEditable = useDashboardStore((state) => state.isEditable);
+
+  return (
+    <div className="flex flex-row gap-3">
+      <h3 className="font-semibold mb-1">{title}</h3>
+      {isEditable && (
+        <Button
+          className="min-w-6 h-6 p-0 text-white bg-success"
+          startContent={<PlusIcon size="20" className="font-black" />}
+        />
+      )}
     </div>
   );
 }
@@ -166,10 +250,27 @@ function ListItem({
   children: ReactNode;
   description?: string;
 }) {
+  const isEditable = useDashboardStore((state) => state.isEditable);
   return (
     <li>
-      <p className="text-gray-900">{children}</p>
-      <span className="text-[12px] text-gray-500">{description}</span>
+      <div className="flex flex-row justify-between">
+        <div>
+          <p className="text-gray-900">{children}</p>
+          <span className="text-[12px] text-gray-500">{description}</span>
+        </div>
+        {isEditable && (
+          <div className="flex flex-row gap-1">
+            <Button
+              className="min-w-5 h-5 p-0 bg-warning-600 text-warning-100"
+              startContent={<EditIcon size="12" />}
+            />
+            <Button
+              className="min-w-5 h-5 p-0 bg-danger-600 text-danger-100"
+              startContent={<CloseIcon fontSize="12" />}
+            />
+          </div>
+        )}
+      </div>
     </li>
   );
 }
@@ -180,21 +281,38 @@ function Medication({
   dosage,
   duration,
   quantity,
+  frequency,
 }: {
   type: string;
   name: string;
   dosage: string;
   duration: string;
-  quantity: string;
+  quantity?: string;
+  frequency: string;
 }) {
+  const isEditable = useDashboardStore((state) => state.isEditable);
   return (
-    <div className="text-sm">
-      <p className="font-semibold">
-        {type} {name} ---- {quantity}
-      </p>
-      <p>
-        {dosage} ---- {duration}
-      </p>
+    <div className="flex flex-row justify-between">
+      <div className="text-sm">
+        <p className="font-semibold">
+          {type} {name} {dosage} ---- {quantity || "40pcs"}
+        </p>
+        <p>
+          {frequency} ---- {duration}
+        </p>
+      </div>
+      {isEditable && (
+        <div className="flex flex-row gap-1">
+          <Button
+            className="min-w-5 h-5 p-0 bg-warning-600 text-warning-100"
+            startContent={<EditIcon size="12" />}
+          />
+          <Button
+            className="min-w-5 h-5 p-0 bg-danger-600 text-danger-100"
+            startContent={<CloseIcon fontSize="12" />}
+          />
+        </div>
+      )}
     </div>
   );
 }
