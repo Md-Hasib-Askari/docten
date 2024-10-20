c6ebb48d6a7e93cf236a15b59cce6967ff7e9da6
Imran1713 <smioni1302@gmail.com>
2024-10-20 10:10:18 +0600

    customer section (landing page) UI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,5 @@
 // "use client";
 
-import CardDemo from "@/components/landing-page/card";
-import Customer from "@/components/landing-page/customer";
-
 export default function Component() {
     return <></>;
 }
