62d4b9bab209b280e78a7d5efabf50b7cc31e1a3
hasib <mdhsbaskari@gmail.com>
2024-11-29 18:34:51 +0600

    added azure vm public url in next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,19 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "5000",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_API_STATIC_URL: process.env.NEXT_PUBLIC_API_STATIC_URL,
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "5000",
+                pathname: "/uploads/**",
+            },
+            {
+                protocol: "http",
+                hostname: "20.6.131.175", // AZURE VM Public URL
+                port: "5000",
+                pathname: "/uploads/**",
+            },
+        ],
+    },
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_API_STATIC_URL: process.env.NEXT_PUBLIC_API_STATIC_URL,
+    },
 };
 
 export default nextConfig;
